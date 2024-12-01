2a4e837dadc520a82e42c27cff75085201d3f0ea
cherubinbila <cherubinbila@fedora>
2024-12-01 03:29:54 +0000

    second commit

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
 </head>
 
 <body>
